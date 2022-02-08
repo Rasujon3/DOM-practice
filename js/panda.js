47df745c13ca773e8cc4607fd6678bc387fcdcea
Rasujon3 <rasujon3@gmail.com>
2022-02-09 00:36:57 +0600

    ২. যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার     কালার lightblue করেদিতে পারো কিনা।

--- js/panda.js
+++ js/panda.js
@@ -1,1 +1,13 @@
-console.log("hi");
+/* 
+    ২. যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার 
+    কালার lightblue করেদিতে পারো কিনা। 
+*/
+const allH2Tag = document.getElementsByTagName("h2");
+// console.log(allH2Tag);
+// allH2Tag[0].style.color = "lightblue";
+// allH2Tag[1].style.color = "lightblue";
+for (const h2Tag of allH2Tag) {
+  console.log(h2Tag);
+  h2Tag.style.color = "lightblue";
+}
+// document.getElementsByTagName("h2").style.color = "lightblue";
